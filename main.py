817166f0afa77c499ac70994ee62290346f82bc8
Dariel Sevilla <darielsevilla@gmail.com>
2024-10-28 12:37:25 -0600

    inicio distribución

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 # This is a sample Python script.
+from numpy import random
+
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def distribucion(tablero):
+    fil = random.randint(9)
+    col = random.randint(9)
+    elemento = random.randint(9)+1
 
+    tableroDistribuido = tablero
+
+
+
+
+
+def inputUsuario(tablero):
+    print("")
 def validacion(tablero, elemento, fil, col):
     #revisando columnas
     analisis = tablero
@@ -18,7 +32,7 @@ def validacion(tablero, elemento, fil, col):
                 if (analisis[i][j] != 0):
                     analisis[i][j] = -1
 
-    int
+
     for regX in range(3):
         for regY in range(3):
             #revisando casillas del cuadrante
@@ -53,7 +67,15 @@ def printTablero(tablero):
 def sudoku():
     tablero = [[0 for i in range(9)] for j in range(9)]
     printTablero(tablero)
-
+    while(victoria(tablero) != False):
+        numero = int(input())
+        fila = int(input())
+        columna = int(input())
+        if validacion(tablero, numero, fila, columna):
+            print("Numero correcto")
+            # poner el numero en la matriz
+        else:
+            print("Numero incorrecto")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
